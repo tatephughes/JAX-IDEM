27cb28ee0a881d73addcc1f4ae19b815caa737a8
tatephughes <etatephughes@gmail.com>
2024-09-25 13:01:10 +0100

    Regular Update

--- src/jax_idem/IDEM.py
+++ src/jax_idem/IDEM.py
@@ -7,60 +7,43 @@ import jax.lax as jl
 import jax.random as rand
 from jax.numpy.linalg import vector_norm, solve
 
+# Plotting imports
+import matplotlib.pyplot as plt
+
 # Statistics and data handling imports
-#import pandas as pd
-#import patsy
+# import pandas as pd
+# import patsy
 
-import utilities
-from utilities import *
+# In-Module imports
+from utilities import create_grid, place_basis, outer_op
 
-def kernel(s, x, thetap):
-
-    """
-    Gaussian kernel function
-
-    Parameters:
-    s: The first coordinate point, should be a JAX array of shape [d,]
-    x: The second coordinate point, should be a JAX array of shape [d,]
-    thetap: an array of the kernel parameters, of the form
-            [scale, shape, *shifts]
-            should be a JAX array of shape [d+2,]
-    """
-
-    scale = thetap[0]
-    shape = thetap[1]
-    shifts = thetap[2:]
-
-    return scale*jnp.exp(-jnp.sum((x - s - shifts)**2)/shape)
 
 def forward_step(beta, kernel, locations, M, key):
-
-    sigma_eta = 0.1 * jnp.exp(-outer_operation(
-        locations, locations, lambda s,x: vector_norm(s-x))
-                              /0.1)
+    sigma_eta = 0.1 * jnp.exp(
+        -outer_op(locations, locations, lambda s, x: vector_norm(s - x)) / 0.1
+    )
     eta = rand.multivariate_normal(key, jnp.zeros(locations.shape[0]), sigma_eta)
 
-def construct_M(kernel, process_basis, grid, griddelta):
 
+def construct_M(kernel, process_basis, grid, gridarea):
     def psi(s, params):
-        squarenorm = jnp.array([jnp.sum((s-params[0:2])**2)])
-        return ((2 - squarenorm)**2 * jnp.where(squarenorm < params[2], 1, 0))[0]
+        squarenorm = jnp.array([jnp.sum((s - params[0:2]) ** 2)])
+        return ((2 - squarenorm) ** 2 * jnp.where(squarenorm < params[2], 1, 0))[0]
 
     # consider a sparse matrix approach here!
     eval_basis = jax.vmap(jax.vmap(psi, in_axes=(None, 0)), in_axes=(0, None))
-    
+
     PHI = eval_basis(grid, process_basis)
 
-    GRAM = (PHI.T @ PHI) * griddelta
+    # griddelta? shouldnt it be the area?
+    GRAM = (PHI.T @ PHI) * gridarea
 
     K = outer_op(grid, grid, kernel)
-    
-    return solve(GRAM, PHI.T @ K @ PHI) * griddelta**2
 
-    
-    
-def simIDEM(T, k, nobs = 100, nres = 2, nint=200):
+    return solve(GRAM, PHI.T @ K @ PHI) * gridarea**2
 
+
+def simIDEM(T, k, K_basis, griddelta=0.01, nobs=100, nres=2, nint=200):
     """
     Simulates from a given IDE model.
     Partially implemented, for now this will use a pre-defined model, similar to AZM's package
@@ -75,63 +58,52 @@ def simIDEM(T, k, nobs = 100, nres = 2, nint=200):
     # key setup
     key = jax.random.PRNGKey(seed=628)
     keys = rand.split(key, 3)
-    
+
     # Bisquare basis functions
     def psi(s, params):
-        squarenorm = jnp.array([jnp.sum((s-params[0:2])**2)])
-        return ((2 - squarenorm)**2 * jnp.where(squarenorm < params[2], 1, 0))[0]
+        squarenorm = jnp.array([jnp.sum((s - params[0:2]) ** 2)])
+        return ((2 - squarenorm) ** 2 * jnp.where(squarenorm < params[2], 1, 0))[0]
+
     vec_phi = jax.vmap(psi, in_axes=(None, 0))
 
     eval_basis = jax.vmap(jax.vmap(psi, in_axes=(None, 0)), in_axes=(0, None))
-    
+
     # Place 90 basis functions across two resolutions (default values are fine here)
     # the original R code bases the grid 100 uniform points across the unit square,
     # which can also be done here by setting data=rand.uniform(<key>, shape=(100,2))
     process_basis = place_basis()
-    r = process_basis.shape[0]
+    nbasis = process_basis.shape[0]
 
     # now, for example, ```eval_basis(<list of points>, params)``` will work
 
     # Other Coefficients
-    beta = jnp.array([0.2,0.2,0.2])
+    beta = jnp.array([0.2, 0.2, 0.2])
     sigma2_eta = 0.01**2
     sigma2_eps = 0.01**2
 
-    Q_eta = jnp.eye(r) / sigma2_eta
-    Q_eps = jnp.eye(nobs*T) / sigma2_eps
-    
+    Q_eta = jnp.eye(nbasis) / sigma2_eta
+    Q_eps = jnp.eye(nobs * T) / sigma2_eps
+
     # Process nodes
-    griddelta = 0.01
-    s_grid = create_grid(jnp.array([[0,1],[0,1]]),
-                         jnp.array([griddelta, griddelta]))
+    s_grid = create_grid(jnp.array([[0, 1], [0, 1]]), jnp.array([griddelta, griddelta]))
 
+    def kernel(s, r):
+        theta = (
+            k[0],
+            k[1],
+            jnp.array([k[2] @ vec_phi(s, K_basis[2]), k[3] @ vec_phi(s, K_basis[3])]),
+        )
 
-    # Basis for spatially variying kernel
-    K_basis = (1, 1, place_basis(nres=2), place_basis(nres=2))
-    #k = (200, 0.002, 0.1*rand.normal(keys[0], shape=(K_basis[2].shape[0], )),
-    #                 0.1*rand.normal(keys[1], shape=(K_basis[3].shape[0], )))
+        return theta[0] * jnp.exp(-(jnp.sum((r - s - theta[2]) ** 2)) / theta[1])
 
-    # experimental values
-    #k = (jnp.array(200),
-    #     jnp.array(0.2),
-    #     0.01*rand.normal(keys[0], shape=(K_basis[2].shape[0], )),
-    #     0.01*rand.normal(keys[1], shape=(K_basis[3].shape[0], )))
-    
-    def kernel(s,r):
-        theta = (k[0], k[1],
-                 jnp.array([k[2] @ vec_phi(s, K_basis[2]),
-                            k[3] @ vec_phi(s, K_basis[3])]))
-    
-        return theta[0] * jnp.exp(-(jnp.sum((r-s-theta[2])**2)) / theta[1])
+    vector = jnp.zeros(nbasis)
+    alpha0 = vector.at[jnp.array([1, 10, 8, 65, 20, 24])].set(1)
 
-    vector = jnp.zeros(r)
-    alpha0 = vector.at[jnp.array([1,10,8, 65, 20, 24])].set(1)
-
-    M = construct_M(kernel, process_basis, s_grid, griddelta)
+    M = construct_M(kernel, process_basis, s_grid, griddelta**2)
 
     def step(carry, key):
-        nextstate = M @ carry + jnp.sqrt(sigma2_eta) * rand.normal(key, shape = (r,))
-        return(nextstate, nextstate)
+        nextstate = M @ carry + jnp.sqrt(sigma2_eta) * rand.normal(key, shape=(nbasis,))
+        return (nextstate, nextstate)
 
     alpha_keys = rand.split(keys[3], T)
 
@@ -139,8 +111,44 @@ def simIDEM(T, k, nobs = 100, nres = 2, nint=200):
 
     def get_process(alpha, s_grid):
         return eval_basis(s_grid, process_basis) @ alpha
-    vget_process = jax.vmap(get_process, in_axes=(0,None))
+
+    vget_process = jax.vmap(get_process, in_axes=(0, None))
 
     process_vals = vget_process(alpha, s_grid)
-    
+
     return process_vals
+
+
+if __name__ == "__main__":
+    print(
+        "IDEM loaded as main. There will be an example simulation here in future versions."
+    )
+
+    key = jax.random.PRNGKey(1)
+    keys = rand.split(key, 3)
+
+    griddelta = 0.01
+    s_grid = create_grid(jnp.array([[0, 1], [0, 1]]), jnp.array([griddelta, griddelta]))
+
+    K_basis = (jnp.array(1), jnp.array(1), place_basis(nres=1), place_basis(nres=1))
+    k = (
+        jnp.array(200),
+        jnp.array(0.002),
+        0.01 * rand.normal(keys[0], shape=(K_basis[2].shape[0],)),
+        0.01 * rand.normal(keys[2], shape=(K_basis[3].shape[0],)),
+    )
+
+    process_vals = simIDEM(T=9, k=k, K_basis=K_basis, griddelta=griddelta)
+
+    fig, axes = plt.subplots(3, 3, figsize=(8, 5))
+
+    for i in range(9):
+        ax = axes[i // 3, i % 3]
+        scatter = ax.scatter(
+            s_grid.T[0], s_grid.T[1], c=process_vals[i], cmap="viridis", marker="s"
+        )
+        ax.set_title(f"T = {i+1}")
+        fig.colorbar(scatter, ax=ax)
+
+    plt.tight_layout()
+    plt.show()
--- src/jax_idem/utilities.py
+++ src/jax_idem/utilities.py
@@ -2,13 +2,12 @@
 
 import jax
 import jax.numpy as jnp
-import jax.lax as jl
-import jax.random as rand
-from jax.numpy.linalg import solve
+# import jax.lax as jl
+# import jax.random as rand
+# from jax.numpy.linalg import solve
 
-def create_grid(bounds,
-                deltas):
 
+def create_grid(bounds, deltas):
     """
     Creates an n-dimensional grid based on the given bounds and deltas.
 
@@ -19,7 +18,7 @@ def create_grid(bounds,
     Returns: Array[Array[Double]]
     JAX array of coordinate arrays for the grid.
     """
-    
+
     dimension = jnp.size(bounds, axis=0)
 
     def gen_range(i):
@@ -31,19 +30,20 @@ def create_grid(bounds,
     # fine for now.
     axis_linspaces = [gen_range(i) for i in range(dimension)]
 
-    grid = jnp.stack(jnp.meshgrid(*axis_linspaces, indexing='ij'),
-                     axis=-1).reshape(-1,dimension)
-    
+    grid = jnp.stack(jnp.meshgrid(*axis_linspaces, indexing="ij"), axis=-1).reshape(
+        -1, dimension
+    )
+
     return grid
 
-def outer_op(a, b, op = lambda x,y: x*y):
 
+def outer_op(a, b, op=lambda x, y: x * y):
     """
     Computes the outer operation of two vectors, a generalisation of the outer product.
 
     Parameters:
-    vec1: Array[A]; array of the first vector
-    vec2: Array[B]; array of the second vector
+    vec1: ArrayLike[A]; array of the first vector
+    vec2: ArrayLike[B]; array of the second vector
     operation: A, B -> C; A function acting on an element of vec1 and an element of vec2.
                By default, this is the outer product.
 
@@ -52,18 +52,15 @@ def outer_op(a, b, op = lambda x,y: x*y):
     two vectors.
     """
 
-    def ascan(aval, bvec):
-        return jl.map(lambda bval: op(aval,bval), bvec)
-    
-    return jl.map(lambda aval: ascan(aval, b), a)
+    vec_op = jax.vmap(jax.vmap(op, in_axes=(None, 0)), in_axes=(0, None))
 
-def place_basis(data = jnp.array([[0,0],[1,1]]),
-                                         nres = 2,
-                                         aperture = 1.25,
-                                         min_knot_num = 3
-                                        ):
+    return vec_op(a, b)
 
-    '''
+
+def place_basis(
+    data=jnp.array([[0, 0], [1, 1]]), nres=2, aperture=1.25, min_knot_num=3
+):
+    """
     Distributes knots and scales for basis functions over a number of resolutions,
     similar to auto_basis from the R package FRK.
     This function must be run outside of a jit loop, since it involves varying the
@@ -72,35 +69,35 @@ def place_basis(data = jnp.array([[0,0],[1,1]]),
     NOTE: This function does not directly give basis functions, like in FRK for
     example. Instead, it returns an array of parameters for a basis function,
     in the form [x_coord, y_coord, scale].
-    
-    
-    '''
 
-    xmin = jnp.min(data[:,0])
-    xmax = jnp.max(data[:,0])
-    ymin = jnp.min(data[:,1])
-    ymax = jnp.max(data[:,1])
-    
-    asp_ratio = (ymax-ymin)/(xmax-xmin)
-    
+
+    """
+
+    xmin = jnp.min(data[:, 0])
+    xmax = jnp.max(data[:, 0])
+    ymin = jnp.min(data[:, 1])
+    ymax = jnp.max(data[:, 1])
+
+    asp_ratio = (ymax - ymin) / (xmax - xmin)
+
     if asp_ratio < 1:
         ny = min_knot_num
-        nx = jnp.round(ny/asp_ratio).astype(int)
+        nx = jnp.round(ny / asp_ratio).astype(int)
     else:
         nx = min_knot_num
-        ny = jnp.round(asp_ratio * nx).astype(int)     
-        
+        ny = jnp.round(asp_ratio * nx).astype(int)
+
     def basis_at_res(res):
+        x_range = jnp.linspace(xmin, xmax, nx * 3**res)
+        y_range = jnp.linspace(ymin, ymax, ny * 3**res)
 
-        x_range = jnp.linspace(xmin, xmax, nx*3**res)
-        y_range = jnp.linspace(ymin, ymax, ny*3**res)
-
-        knots = jnp.stack(jnp.meshgrid(x_range, y_range, indexing='ij'),
-                 axis=-1).reshape(-1,2)
+        knots = jnp.stack(
+            jnp.meshgrid(x_range, y_range, indexing="ij"), axis=-1
+        ).reshape(-1, 2)
 
         def pairwise_distances(points):
             diff = points[:, None, :] - points[None, :, :]
-            dist = jnp.sqrt(jnp.sum(diff ** 2, axis=-1))
+            dist = jnp.sqrt(jnp.sum(diff**2, axis=-1))
             return dist
 
         distances = pairwise_distances(knots)
@@ -110,7 +107,7 @@ def place_basis(data = jnp.array([[0,0],[1,1]]),
 
         aperture_res = min_distance * aperture
 
-        return  jnp.hstack([knots, jnp.full((knots.shape[0], 1), aperture_res)])
+        return jnp.hstack([knots, jnp.full((knots.shape[0], 1), aperture_res)])
 
     # You cant abstract over array lengths, this must(?) be done like this
     return jnp.vstack([basis_at_res(res) for res in range(nres)])
